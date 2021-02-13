1b460011c60afbf761e278297558783203cc0287
willh33 <wharrison33@hotmail.com>
2021-02-13 14:52:19 -0700

    added more project images

--- client/src/components/Portfolio.js
+++ client/src/components/Portfolio.js
@@ -39,6 +39,26 @@ function Portfolio(props) {
 					</div>
 				</div>
 				<div className="row">
+					<div className="col-lg-12">
+						<h4 className="text-info">Employee Directory</h4>
+						<img src="./Assets/Images/employee-directory.png" id="reserve-a-room-img" className="project-img portfolio-img" alt="..." />
+						<div className="pb-3">
+							<a href="https://github.com/willh33/employee_directory_will_h" className="project-img portfolio-img">Github Repo</a>
+							<a href="https://willh33.github.io/employee_directory_will_h/" className="project-img portfolio-img">Deployed Application</a>
+						</div>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-lg-12">
+						<h4 className="text-info">Budget Tracker</h4>
+						<img src="./Assets/Images/budget-tracker.png" id="budget-tracker-img" className="project-img portfolio-img" alt="..." />
+						<div className="pb-3">
+							<a href="https://github.com/willh33/will_h_budget_tracker" className="project-img portfolio-img">Github Repo</a>
+							<a href="https://budget-tracker-wh.herokuapp.com/" className="project-img portfolio-img">Deployed Application</a>
+						</div>
+					</div>
+				</div>
+				<div className="row">
 					<div className="col-lg-6">
 							<img src="./Assets/Images/grand-america.jpg" className="portfolio-img" alt="..."/>
 					</div>
